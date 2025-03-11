b34c3b80792ae425b2edae6f85ccfaea06d25b98
Donal260202 <116863910+Donal260202@users.noreply.github.com>
2025-03-11 15:48:34 +0530

    mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="PMFBY ANALYSIS",layout="wide")
 df=pd.read_excel("PMFBY Taluka 18-23.xlsx")
 
 def header_page():
-    st.markdown("<h1 style='text-align: center; color: black;'>Analysis of PMFBY Data 2018-2023</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: red;'>Analysis of PMFBY Data 2018-2023</h1>", unsafe_allow_html=True)
 
     st.markdown("<h2 style='text-align: center; color: grey;'>By: Ashwin Chaudhari</h2>", unsafe_allow_html=True)
     
